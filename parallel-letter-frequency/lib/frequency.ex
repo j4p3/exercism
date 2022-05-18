d8b1c36f3e0b3f8d71ed7fc6c9cd7a46e372874e
JP <jp.bonner@bonner.jp>
2022-05-17 20:14:35 -0400

    parallel letter frequency: add edge case handling

--- parallel-letter-frequency/lib/frequency.ex
+++ parallel-letter-frequency/lib/frequency.ex
@@ -1,5 +1,7 @@
 defmodule Frequency do
   @doc """
+  https://exercism.org/tracks/elixir/exercises/parallel-letter-frequency
+
   Count letter frequency in parallel.
 
   Returns a map of characters to frequencies.
@@ -7,20 +9,33 @@ defmodule Frequency do
   The number of worker processes to use can be set with 'workers'.
   """
   @spec frequency([String.t()], pos_integer) :: map
+  def frequency([], _workers), do: %{}
+
   def frequency(texts, workers) do
     texts
     |> Enum.chunk_every(ceil(length(texts) / workers))
     |> Enum.map(&Task.async(fn -> frequency_count(&1) end))
     |> Enum.map(&Task.await/1)
-    |> Enum.reduce(&Map.merge(&1, &2, fn k, v1, v2 -> v1 + v2 end))
+    |> Enum.reduce(&Map.merge(&1, &2, fn _k, v1, v2 -> v1 + v2 end))
   end
 
-  def frequency_count(texts) do
-    # @todo: merge upper/lowercase letters
+  defp frequency_count(texts) do
     Enum.reduce(texts, %{}, fn text, acc ->
-      for <<i::binary-size(1) <- String.downcase(text)>>, i in ?a..?z, reduce: acc do
-        acc -> Map.update(acc, i, 1, &(&1 + 1))
-      end
+      text
+      |> String.trim()
+      |> String.downcase()
+      |> String.replace([",", "."], "")
+      |> String.replace(~r/[[:digit:]]/, "")
+      |> String.codepoints()
+      |> Enum.reject(&blank?/1)
+      |> Enum.reduce(acc, fn char, acc ->
+        Map.update(acc, char, 1, &(&1 + 1))
+      end)
     end)
   end
+
+  defp blank?(nil), do: true
+  defp blank?(""), do: true
+  defp blank?(val) when is_binary(val), do: String.trim(val) == ""
+  defp blank?(_val), do: false
 end
